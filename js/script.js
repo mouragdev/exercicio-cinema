f93efc16a879e71d6349f077bfcc1e419eb6c785
Gabriel Moura <gmoura.dev@outlook.com>
2026-07-10 12:24:19 -0300

    Refatorando o Script

--- js/script.js
+++ js/script.js
@@ -1,44 +1,34 @@
 function calcularTempo() {
 
+
+
 // Entrada
 
-let nome = document.getElementById("nome-filme").value;
-let resultadoNome = document.getElementById("resultado-nome");
+const titulo = document.getElementById("titulo").value;
+const duracao = document.getElementById("duracao").valueAsNumber;
 
-let minutos = document.getElementById("tempo-filme").valueAsNumber;
-let resultadoTempo = document.getElementById("resultado-tempo");
 
 // Processamento
 
-let horas = Math.trunc(minutos / 60)
-let sobra = minutos % 60;
+let hora = Math.trunc(duracao / 60)
+let minuto = duracao % 60
 
-let textoHora = horas == 1 ? "Hora" : "Horas";
-let textoMinuto = sobra == 1 ? "Minuto" : "Minutos";
+let textoHora = hora == 1 ? "Hora" : "Horas";
+let textoMinuto = minuto == 1 ? "Minuto" : "Minutos";
+
+
 
 // Saída
 
-resultadoNome.textContent = "FIlme: " + nome;
+document.getElementById("resultado-nome").textContent = `Filme: ${titulo}`
 
-if (horas == 0) {
-    resultadoTempo.textContent = `Duração: ${sobra} ${textoMinuto}`
-} else if (sobra == 0) {
-    resultadoTempo.textContent = `Duração: ${horas} ${textoHora}`
+if (hora == 0) {
+    document.getElementById("resultado-tempo").textContent = `Ducação: ${minuto} ${textoMinuto}.`
+} else if (minuto == 0) {
+    document.getElementById("resultado-tempo").textContent = `Ducação: ${hora} ${textoHora}.`
 } else {
-    resultadoTempo.textContent = `Duração: ${horas} ${textoHora} e ${sobra} ${textoMinuto}.`
+    document.getElementById("resultado-tempo").textContent = `Ducação: ${hora} ${textoHora} e ${minuto} ${textoMinuto}.`
 }
 
+
 }
-
-
-// if (horas == 0) {
-//     resultadoTempo.textContent = `Duração: ${sobra} Minuto(s).`
-// } else if (horas == 1 && sobra <= 0) {
-//     resultadoTempo.textContent = `Duração: ${horas} Hora.`
-// } else if (horas == 1 && sobra >= 1) {
-//     resultadoTempo.textContent = `Duração: ${horas} Hora e ${sobra} Minutos.`
-// } else if (horas > 1 && sobra <= 0) {
-//     resultadoTempo.textContent = `Duração: ${horas} Horas.`
-// } else if (horas > 1 && sobra >= 1) {
-//     resultadoTempo.textContent = `Duração: ${horas} Horas e ${sobra} Minutos.`
-// }
